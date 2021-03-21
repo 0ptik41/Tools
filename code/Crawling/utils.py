a80efc2648db2d176b98eb5b97415526ca652015
TylersDurden <0ptik41@protonmail.com>
2021-03-21 09:35:16 -0400

    small fix to help from accumulating junk files

--- code/Crawling/utils.py
+++ code/Crawling/utils.py
@@ -24,3 +24,18 @@ def create_random_filename(ext):
 	for n in range(9): pool.append(str(n))
 	random.shuffle(pool)
 	return ''.join(random.sample(pool, 6)) + ext
+
+
+def create_timestamp():
+    date = time.localtime(time.time())
+    mo = str(date.tm_mon)
+    day = str(date.tm_mday)
+    yr = str(date.tm_year)
+
+    hr = str(date.tm_hour)
+    min = str(date.tm_min)
+    sec = str(date.tm_sec)
+
+    date = mo + '/' + day + '/' + yr
+    timestamp = hr + ':' + min + ':' + sec
+    return date, timestamp
